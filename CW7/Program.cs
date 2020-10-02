cf9659402e9a24824796105d0d203b60de4611cd
Enrique Tejeda <enrgteje@ut.utm.edu>
2020-10-02 12:52:03 -0500

    test 2

--- CW7/Program.cs
+++ CW7/Program.cs
@@ -16,14 +16,23 @@ namespace CW7
             int height = 2;
 
             Widget w = new TextField(width, height);
-            Decorator border = new BorderDecorator(w);
-            Decorator scroll = new ScrollDecorator(w);
+            BorderDecorator border = new BorderDecorator(w);
+            ScrollDecorator scroll = new ScrollDecorator(w);
             Console.WriteLine("W draw");
             w.draw();
             Console.WriteLine("Border draw");
             border.draw();
             Console.WriteLine("Scroll draw");
             scroll.draw();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            BorderDecorator border2 = new BorderDecorator(w);
+            ScrollDecorator scroll2 = new ScrollDecorator(border2);
+            border2.draw();
+            scroll2.draw();
         }
     }
 }
